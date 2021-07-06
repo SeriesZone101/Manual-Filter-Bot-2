1808195ed3094ed9576007934ae99e1b01d1d750
SeriesZone101 <85978656+SeriesZone101@users.noreply.github.com>
2021-07-06 11:28:08 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Developer", url="https://t.me/DeepakV111"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/serieszOne3"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/movieszzzone")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://github.com/SeriesZone101/Manual-Filter-Bot-2")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
