1af420ea461560ac7dcb49af4ed866daf3039a16
SeriesZone101 <85978656+SeriesZone101@users.noreply.github.com>
2021-07-06 11:31:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Developer", url="https://t.me/DeepakV111"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/serieszOne3"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/movieszzzone")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://github.com/SeriesZone101/Manual-Filter-Bot-2")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
